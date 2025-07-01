26b68654805bcd8928442b58d11730fac7f26d58
OllieSHunt <100690050+OllieSHunt@users.noreply.github.com>
2025-07-01 15:03:28 +0100

    Added keys to the layers
    Now a funcionting keyboard!

--- keyboards/custom_keyboard_v2/keymaps/default/keymap.c
+++ keyboards/custom_keyboard_v2/keymaps/default/keymap.c
@@ -18,7 +18,7 @@
 static painter_device_t display;
 
 static uint16_t display_sleep_timer; // I handle display sleep manually because QUANTUM_PAINTER_DISPLAY_TIMEOUT was not working for some reason
-#define DISPLAY_TIMEOUT 3000
+#define DISPLAY_TIMEOUT 5000
 #define DISPLAY_TIMEOUT_CHECK_FREQ 1000
 
 // Handles to images
@@ -38,36 +38,37 @@ enum KeyboardLayers {
 };
 
 // Keymap
+// IMPORTANT: This keymap is intended to work with an OS keyboard layout of UK QWERTY not US QWERTY.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DVK] = LAYOUT_ortho_5x10(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_NO,   KC_NO,   _______, _______, _______, _______, TT(SYM), _______, KC_NO,   KC_NO
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
+        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
+        KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
+        KC_NO,   KC_NO,   KC_LALT, KC_SPC,  KC_LCMD, KC_LCTL, TT(SYM), KC_LSFT, KC_NO,   KC_NO
     ),
 
     [QWT] = LAYOUT_ortho_5x10(
-        _______, _______, PDF(DVK),_______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
         KC_NO,   KC_NO,   _______, _______, _______, _______, _______, _______, KC_NO,   KC_NO
     ),
 
     [SYM] = LAYOUT_ortho_5x10(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_PSCR, KC_PSLS, KC_PAST, KC_NO,   KC_APP,
+        KC_BSLS, KC_GRV,  KC_LBRC, KC_RBRC, KC_ESC,  KC_NUM,  KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
+        KC_NUBS, KC_SLSH, KC_DEL,  KC_BSPC, KC_ENT,  KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_NO,   KC_MINS, KC_EQL,  KC_TAB,  KC_NO,   KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_NO,   KC_NO,   _______, TT(CTL), _______, _______, _______, _______, KC_NO,   KC_NO
     ),
 
     [CTL] = LAYOUT_ortho_5x10(
-        _______, _______, _______, _______, _______, _______, _______, _______, EE_CLR,  QK_BOOT,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, PDF(DVK),PDF(QWT),_______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_NO,   EE_CLR,  QK_BOOT,
+        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  
+        KC_F11,  KC_F12,  KC_INS,  KC_HOME, KC_PGUP, KC_RALT, PDF(QWT),PDF(DVK),KC_NO,   KC_NO,  
+        KC_SCRL, KC_PAUS, KC_NO,   KC_END,  KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  
         KC_NO,   KC_NO,   _______, TT(CTL), _______, _______, _______, _______, KC_NO,   KC_NO
     ),
 
@@ -108,6 +109,8 @@ void draw_layer_image(layer_state_t state) {
 
 // Redraw the whole screen
 void draw_whole_screen(void) {
+    // TODO: Add num lock, caps lock, and scroll lock indicators
+
     // Static UI elements that do not change
     qp_drawimage(display, 0, 0, static_ui);
     
