7dfa02214fef84f55e9c8cc8ee3030cb3ce855d1
OllieSHunt <100690050+OllieSHunt@users.noreply.github.com>
2026-07-31 20:58:07 +0100

    Slightly more VIM like keybinds

--- keyboards/custom_keyboard_v2/keymaps/default/keymap.c
+++ keyboards/custom_keyboard_v2/keymaps/default/keymap.c
@@ -92,15 +92,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [SYM] = LAYOUT_ortho_5x10(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         KC_F11,  KC_GRV,  KC_MINS, KC_LBRC, KC_NUBS, KC_NUHS, KC_RBRC, KC_EQL,  KC_QUOT, KC_F12,
-        KC_TAB,  KC_DEL,  KC_BSPC, KC_ENT,  KC_ESC,  KC_APP,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        KC_ESC,  KC_TAB,  KC_DEL,  KC_BSPC, KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_APP,
         _______, _______, _______, _______, _______, _______, KC_CAPS, KC_NUM,  KC_SCRL, _______,
         KC_NO,   KC_NO,   _______, TT(CTL), _______, _______, TG(SYM), _______, KC_NO,   KC_NO
     ),
 
     [CTL] = LAYOUT_ortho_5x10(
         RM_PREV, RM_SPDD, RM_VALD, RM_SATD, RM_HUED, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, RM_NEXT,
-        KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, PDF(QWT),PDF(DVK),KC_NO,   RM_TOGG,
-        KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_PENT, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PENT, PDF(QWT),PDF(DVK),KC_NO,   RM_TOGG,
+        KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,
         KC_PSLS, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PSCR, KC_INS,  KC_PAUS, EE_CLR,  QK_BOOT,
         KC_NO,   KC_NO,   _______, TG(CTL), _______, _______, KC_NO,   _______, KC_NO,   KC_NO
     ),
